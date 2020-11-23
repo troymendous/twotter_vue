5036a7b773ddc727ed84f3d1dc94b55f470fae5a
Troymendous <hello@troywilson.io>
2020-11-23 21:44:48 +1100

    Finishing tweet submit button and functions

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -9,6 +9,24 @@
         <div class="user-profile__follower-count">
             <strong>Followers</strong> {{ followers }}
         </div>
+        <form action="" class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+          <label for="newTwoot"> <strong>New Twoot</strong></label>
+          <textarea name="" id="newTwoot" rows="4" v-model="newTwootContent"/>
+
+          <div class="user-profile__create-twoot-type">
+            <label for="newTwootType">
+              <strong>Type:</strong>
+            </label>
+            <select id="newTwootType" v-model="selectedTwootType">
+              <option :value="option.value" v-for="(option, index) in twootTypes" :key="index" >
+                {{ option.name }}
+              </option>
+            </select>
+          </div>
+          <button>
+            Twoot!
+          </button>
+        </form>
     </div>
     <div class="user-profile__twoots-wrapper">
       <TwootItem v-for="twoot in user.twoots" :key="twoot.id" :username="user.username" :twoot="twoot" @favourite="toggleFavourite"/>
@@ -33,6 +51,12 @@ export default {
   name: "UserProfile",
   data() {
     return {
+      newTwootContent: '',
+      selectedTwootType: 'instant',
+      twootTypes: [
+        { value: 'draft', name: 'Draft' },
+        { value: 'instant', name: 'Instant Twoot'}
+        ],
       followers: 0,
       user: {
         id: 1,
@@ -78,7 +102,17 @@ export default {
   },
   toggleFavourite(id) {
     console.log(`favourited Tweet #${id}`)
-  }
+  },
+
+  createNewTwoot() {
+    if (this.newTwootContent && this.selectedTwootType !== 'draft') {
+      this.user.twoots.unshift( items, {
+        id: this.user.twoots.length + 1,
+        content: this.newTwootContent
+       })
+    }
+
+  },
 }
 
 </script>
@@ -108,9 +142,17 @@ export default {
   margin-right: auto;
   padding: 0 10px;
   font-weight: bold;
+  margin-bottom: 20px;
 }
 
 h1 {
     margin: 0;
+}
+
+.user-profile__create-twoot {
+  padding-top: 24px;
+  border-top: 1px solid #DFE3E8;
+  display: flex;
+  flex-direction: column;
 }
 </style>
